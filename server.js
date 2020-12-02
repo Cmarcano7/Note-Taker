136963be7f5de39e7e334c3ca72f9fedbfc42e28
CMarcano7 <tino93pr@hotmail.com>
2020-12-01 23:19:52 -0500

    Initial Commit

---
+++ server.js
@@ -0,0 +1,30 @@
+// Getting all dependencies
+
+var express = require("express");
+var fs = require("fs");
+var database = require("./db/db.json")
+
+var app = express();
+
+// Sets an initial port. We"ll use this later in our listener
+var PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.get("/*", function(req, res) {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
+app.get("/api/notes", function(req, res) {
+    return res.json(database);
+});
